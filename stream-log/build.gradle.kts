1338236d85b958acc18b9574c5318c66dd146cc1
skydoves <skydoves@naver.com>
2022-12-08 15:19:18 +0900

    Define java version to 1.8 for stream-log and file modules

--- stream-log/build.gradle.kts
+++ stream-log/build.gradle.kts
@@ -13,6 +13,12 @@ rootProject.extra.apply {
 
 apply(from = "$rootDir/scripts/publish-module.gradle")
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+
 dependencies {
     testImplementation(Dependencies.junit4)
     detektPlugins(Dependencies.detektFormatting)
