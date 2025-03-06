b4949e9a328d992eab5097fbddf1834cee562621
Jc Miñarro <josecarlos.minarro@gmail.com>
2025-03-06 12:05:01 +0100

    Upgrade Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ subprojects {
     configure<com.diffplug.gradle.spotless.SpotlessExtension> {
         kotlin {
             target("**/*.kt")
-            targetExclude("$buildDir/**/*.kt")
+            targetExclude("${layout.buildDirectory}/**/*.kt")
             ktlint().editorConfigOverride(
                 mapOf(
                     "indent_size" to "2",
